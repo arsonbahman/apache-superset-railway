48c427836484e04b303ff55dfd8071563fb58896
Ju Li <ju.li@stern.nyu.edu>
2025-05-15 14:46:44 -0400

    public embed flags

--- config/superset_config.py
+++ config/superset_config.py
@@ -2,6 +2,8 @@ import os
 
 FEATURE_FLAGS = {
     "ENABLE_TEMPLATE_PROCESSING": True,
+    "EMBEDDED_SUPERSET": True,
+    "DASHBOARD_RBAC": True,
 }
 
 ENABLE_PROXY_FIX = True
@@ -9,3 +11,12 @@ ENABLE_PROXY_FIX = True
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE")
+
+TALISMAN_ENABLED = True
+TALISMAN_CONFIG = {
+    "content_security_policy": {
+        "frame-ancestors": ["*"],
+    },
+}
+
+PUBLIC_ROLE_LIKE = "Gamma"
