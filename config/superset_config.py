8537f79559ab83670998e14dc261415b07506ccb
Ju Li <ju.li@stern.nyu.edu>
2025-05-22 10:57:07 -0400

    add languages

--- config/superset_config.py
+++ config/superset_config.py
@@ -6,4 +6,10 @@ FEATURE_FLAGS = {
 
 ENABLE_PROXY_FIX = True
 
+LANGUAGES = {
+    'en': {'flag': 'us', 'name': 'English'},
+    'fr': {'flag': 'fr', 'name': 'French'},
+    'zh': {'flag': 'cn', 'name': 'Chinese'},
+}
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
